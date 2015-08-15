596929f9e40c7d015f988bed4f15bb938e7aaca3
Paul Trippett <paul@trippett.org>
2015-08-15 20:15:49 +0200

    Remove code duplication in namespace autoloaders

--- lib/endicia_label_server.rb
+++ lib/endicia_label_server.rb
@@ -8,21 +8,19 @@ module EndiciaLabelServer
   autoload :Exceptions,            "#{GEM_NAME}/exceptions"
   autoload :Util,                  "#{GEM_NAME}/util"
 
-  module Builders
-    autoload :BuilderBase,             "#{GEM_NAME}/builders/builder_base"
-    autoload :PostageRateBuilder,      "#{GEM_NAME}/builders/postage_rate_builder"
-    autoload :PostageRatesBuilder,     "#{GEM_NAME}/builders/postage_rates_builder"
-    autoload :UserSignUpBuilder,       "#{GEM_NAME}/builders/user_sign_up_builder"
-    autoload :PostageLabelBuilder,     "#{GEM_NAME}/builders/postage_label_builder"
-    autoload :ChangePassPhraseBuilder, "#{GEM_NAME}/builders/change_pass_phrase_builder"
-  end
 
-  module Parsers
-    autoload :ParserBase,              "#{GEM_NAME}/parsers/parser_base"
-    autoload :PostageRateParser,       "#{GEM_NAME}/parsers/postage_rate_parser"
-    autoload :PostageRatesParser,      "#{GEM_NAME}/parsers/postage_rates_parser"
-    autoload :UserSignUpParser,        "#{GEM_NAME}/parsers/user_sign_up_parser"
-    autoload :PostageLabelParser,      "#{GEM_NAME}/parsers/postage_label_parser"
-    autoload :ChangePassPhraseParser,  "#{GEM_NAME}/parsers/change_pass_phrase_parser"
+  ['Builder', 'Parser'].each do |mod|
+    defined_module = EndiciaLabelServer.const_set("#{mod}s".to_sym, Module.new)
+    defined_module.autoload "#{mod}Base", "#{GEM_NAME}/#{mod.downcase}s/#{mod.downcase}_base"
+
+    [
+      'postage_rate',
+      'postage_rates',
+      'user_sign_up',
+      'postage_label',
+      'change_pass_phrase'
+    ].each do |klass|
+      defined_module.autoload "#{Util::camelize(klass)}#{mod}", "#{GEM_NAME}/#{mod.downcase}s/#{klass}_#{mod.downcase}"
+    end
   end
 end
